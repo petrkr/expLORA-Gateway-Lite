bc4d2f9dec3d1467193a3c1192c57b6ed8f38df9
Petr Kracík <petrkr@petrkr.net>
2025-05-20 16:28:54 +0200

    Move board specific config to variant

---
+++ variants/esp32-s3-devkitc-1/board_config.h
@@ -0,0 +1,13 @@
+#ifndef BOARD_CONFIG_H_
+#define BOARD_CONFIG_H_
+
+// Hardware configuration
+// RFM95W pin definitions
+#define LORA_CS 5       // NSS pin
+#define LORA_RST 14     // RESET pin
+#define LORA_DIO0 36    // DIO0 pin (interrupt)
+#define SPI_MISO_PIN 35 // MISO pin
+#define SPI_MOSI_PIN 37 // MOSI pin
+#define SPI_SCK_PIN 18  // SCK pin
+
+#endif
