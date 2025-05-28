011e040f634e7af326218c71c19a66741c5651bd
Petr Kracík <petrkr@petrkr.net>
2025-05-28 15:15:22 +0200

    Variant: OctopusLab ESP32 board with LAN Phy

---
+++ variants/octopuslab-esp32board-lanphy/board_config.h
@@ -0,0 +1,13 @@
+#ifndef BOARD_CONFIG_H_
+#define BOARD_CONFIG_H_
+
+// Hardware configuration
+// RFM95W pin definitions
+#define LORA_CS       5  // NSS pin
+#define LORA_RST     -1  // RESET pin
+#define LORA_DIO0    39  // DIO0 pin (interrupt)
+#define SPI_MISO_PIN 35  // MISO pin
+#define SPI_MOSI_PIN 32  // MOSI pin
+#define SPI_SCK_PIN  33  // SCK pin
+
+#endif
