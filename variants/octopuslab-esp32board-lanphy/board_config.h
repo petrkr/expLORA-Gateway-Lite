c7e1720d70497e8595f8a180112210eae4f369a8
Petr Kracík <petrkr@petrkr.net>
2025-05-28 18:24:40 +0200

    Fix olab board pinout

--- variants/octopuslab-esp32board-lanphy/board_config.h
+++ variants/octopuslab-esp32board-lanphy/board_config.h
@@ -5,7 +5,7 @@
 // RFM95W pin definitions
 #define LORA_CS       5  // NSS pin
 #define LORA_RST     -1  // RESET pin
-#define LORA_DIO0    39  // DIO0 pin (interrupt)
+#define LORA_DIO0    34  // DIO0 pin (interrupt)
 #define SPI_MISO_PIN 35  // MISO pin
 #define SPI_MOSI_PIN 32  // MOSI pin
 #define SPI_SCK_PIN  33  // SCK pin
